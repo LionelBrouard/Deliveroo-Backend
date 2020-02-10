f144894cf30acfb0be3d2d3e7d07ef7afa8f83e6
LionelBrouard <lionel.brouard@gmail.com>
2020-02-10 14:16:04 +0100

    added .env file

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const cors = require("cors");
-const express = require("express");
-const cors = require("cors");
 
 const app = express();
 
 app.use(cors());
+
 app.get("/", (req, res) => {
   return res.json({
     restaurant: {
@@ -452,16 +451,7 @@ app.get("/", (req, res) => {
     ]
   });
 });
-// Heroku va nous fournir une variable process.env.PORT
-// if (process.env.PORT) {
-//   app.listen(process.env.PORT, () => {
-//     console.log("Server started");
-//   });
-// } else {
-//   app.listen(3200, () => {
-//     console.log("Server started");
-//   });
-// }
+
 app.listen(process.env.PORT || 3200, () => {
   console.log("Server started");
 });
